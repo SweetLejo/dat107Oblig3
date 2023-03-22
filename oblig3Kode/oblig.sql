11e670e418574c865bdbf33408c1ef493a26c97d
leo <heldal.leonard@gmail.com>
2023-03-22 14:34:35 +0100

    sql start

--- oblig3Kode/oblig.sql
+++ oblig3Kode/oblig.sql
@@ -1,1 +1,34 @@
+create schema oblig;
+set search_path to oblig;
+
+create table employee
+(
+  employee_id serial primary key,
+  unique_name varchar(4) check(length(name) >= 3),
+  first_name varchar(30),
+  last_name varchar(30),
+  employment_date date,
+  stilling varchar(30),
+  wage numeric(10, 2),
+  department int,
+  forgein key (department_id) references department(department_id),
+  project int forgein key references project(id)
+  );
+
+create table departemnt
+(
+  department_id serial primary key,
+  department_name varchar(30),
+  boss_id int references emplyee_id
+
+);
+
+create table project
+(
+  project_id serial primary key,
+  project_name varchar(30),
+  beskrivelse TEXT,
+  number_of_hours int,
+  employee_id int references employee(employee_id),
+);
 
