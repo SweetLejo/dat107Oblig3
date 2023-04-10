540efc1d2909167cd5a8c53e8109fc4962f06b76
leo <heldal.leonard@gmail.com>
2023-04-10 14:41:21 +0200

    persistance update

--- oblig3Kode/src/Main.java
+++ oblig3Kode/src/Main.java
@@ -1,5 +1,15 @@
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private String url = System.getenv("DB_URL");
+    private String user = System.getenv("DB_USER");
+    private String password = System.getenv("DB_PASSWORD");
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+    public static void main(String[] args){
+        
     }
+
 }
